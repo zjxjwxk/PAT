8ea92dc8967369f040b0d76e895ad7bda0dc0609
zjxjwxk <zjxjwxk@gmail.com>
2019-12-24 10:47:56 +0800

    update B1022

--- C++/Basic Level/B1022.cpp
+++ C++/Basic Level/B1022.cpp
@@ -1,17 +1,17 @@
 #include <cstdio>
 
 int main() {
-	int a, b, d;
-	scanf("%d %d %d", &a, &b, &d);
-	int sum = a + b;
-	int num[31];
-	int i = 0;
-	do {
-		num[i++] = sum % d;
-		sum /= d;
-	} while(sum != 0);
-	for (i = i - 1; i >= 0; i--) {
-		printf("%d", num[i]);
-	}
-	return 0;
+    int a, b, d;
+    scanf("%d %d %d", &a, &b, &d);
+    int sum = a + b;
+    int num[31];
+    int i = 0;
+    do {
+        num[i++] = sum % d;
+        sum /= d;
+    } while(sum != 0);
+    for (i = i - 1; i >= 0; i--) {
+        printf("%d", num[i]);
+    }
+    return 0;
 }
