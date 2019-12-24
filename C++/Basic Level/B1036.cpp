d2b9a839931f8fe0b109305ea0b506fafc523e46
zjxjwxk <zjxjwxk@gmail.com>
2019-12-24 10:53:39 +0800

    update

--- C++/Basic Level/B1036.cpp
+++ C++/Basic Level/B1036.cpp
@@ -1,20 +1,20 @@
 #include <cstdio>
 
 int main() {
-	int n, row;
-	char c;
-	scanf("%d %c", &n, &c);
-	row = n * 0.5 + 0.5;
-	for (int i = 0; i < row; i++) {
-		printf("%c", c);
-		for (int j = 1; j < n - 1; j++) {
-			if (i == 0 || i == row - 1) {
-				printf("%c", c);
-			} else {
-				printf(" ");
-			}
-		}
-		printf("%c\n", c);
-	}
-	return 0;
+    int n, row;
+    char c;
+    scanf("%d %c", &n, &c);
+    row = n * 0.5 + 0.5;
+    for (int i = 0; i < row; i++) {
+        printf("%c", c);
+        for (int j = 1; j < n - 1; j++) {
+            if (i == 0 || i == row - 1) {
+                printf("%c", c);
+            } else {
+                printf(" ");
+            }
+        }
+        printf("%c\n", c);
+    }
+    return 0;
 } 
