b1adb9f8d9a8efc6b1ae0746cd2a48992e4fc421
zjxjwxk <zjxjwxk@gmail.com>
2019-12-24 10:50:27 +0800

    update

--- C++/Basic Level/B1001.cpp
+++ C++/Basic Level/B1001.cpp
@@ -1,16 +1,16 @@
 #include <cstdio>
 
 int main() {
-	int n, count = 0; 
-	scanf("%d", &n);
-	while (n != 1) {
-		if (n % 2 == 0) {
-			n /= 2;
-		} else {
-			n = (n * 3 + 1) / 2;
-		}
-		count++;
-	}
-	printf("%d", count);
-	return 0;
+    int n, count = 0; 
+    scanf("%d", &n);
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = (n * 3 + 1) / 2;
+        }
+        count++;
+    }
+    printf("%d", count);
+    return 0;
 }
