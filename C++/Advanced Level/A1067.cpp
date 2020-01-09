2b10b9a1c06e9b7871bb01e1642e7da9fb45fa76
zjxjwxk <zjxjwxk@gmail.com>
2020-01-09 23:15:37 +0800

    add A1067

--- C++/Advanced Level/A1067.cpp
+++ C++/Advanced Level/A1067.cpp
@@ -5,15 +5,13 @@ using namespace std;
 int main() {
     int n;
     scanf("%d", &n);
-    int num[100000];
     int index[100000];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &num[i]);
-        index[num[i]] = i;
-    }
     int ans = n - 1;
     for (int i = 0; i < n; i++) {
-        if (num[i] != 0 && num[i] == i) {
+        int num;
+        scanf("%d", &num);
+        index[num] = i;
+        if (num != 0 && num == i) {
             ans--;
         }
     }
@@ -21,22 +19,15 @@ int main() {
     for (int i = 0; i < ans; i++) {
         if (index[0] == 0) {
             for (int j = k; j < n; j++) {
-                if (num[j] != j) {
-                    num[0] = num[j];
-                    num[j] = 0;
-                    index[0] = j;
-                    index[num[0]] = 0;
+                if (index[j] != j) {
+                    swap(index[0], index[j]);
                     k = j;
                     break;
                 }
             }
             ans++;
         } else {
-            num[index[0]] = index[0];
-            num[index[index[0]]] = 0;
-            int temp = index[0];
-            index[0] = index[index[0]];
-            index[temp] = temp;
+            swap(index[0], index[index[0]]);
         }
     }
     printf("%d", ans);
