a3a068120cb04cd0e19ce65268331197a68a29ad
zjxjwxk <zjxjwxk@gmail.com>
2020-01-11 15:44:09 +0800

    add A1085

--- C++/Advanced Level/A1085.cpp
+++ C++/Advanced Level/A1085.cpp
@@ -1,3 +1,4 @@
+// binary search
 #include <cstdio>
 #include <algorithm>
 using namespace std;
@@ -21,3 +22,34 @@ int main() {
     printf("%d", max);
     return 0;
 }
+
+/* 
+// tow pointers
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    long long p;
+    scanf("%d %lld", &n, &p);
+    long long num[100000];
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &num[i]);
+    }
+    sort(num, num + n);
+    int i = 0, j = 1, max_num = 1;
+    while (j <= n - 1) {
+        if (num[i] * p >= num[j]) {
+            if (j - i + 1 > max_num) {
+                max_num = j - i + 1;
+            }
+            j++;
+        } else {
+            i++;
+        }
+    }
+    printf("%lld", max_num);
+    return 0;
+}
+*/
