cd3d2af936b6e8d1d11a32f94ea24a2cc5277a46
zjxjwxk <zjxjwxk@gmail.com>
2020-01-14 22:11:02 +0800

    add B1008

--- C++/Basic Level/B1008.cpp
+++ C++/Basic Level/B1008.cpp
@@ -1,5 +1,45 @@
 #include <cstdio>
 
+// Solution on book
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return gcd(b, a % b);
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int num[100];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &num[i]);
+    }
+    m = m % n;
+    if (m != 0) {
+        int d = gcd(n, m);
+        for (int i = n - m; i <= n - m + d - 1; i++) {
+            int temp = num[i];
+            int now = i;
+            while ((now - m + n) % n != i) {
+                num[now] = num[(now - m + n) % n];
+                now = (now - m + n) % n;
+            }
+            num[now] = temp;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d", num[i]);
+        if (i != n - 1) {
+            printf(" ");
+        }
+    }
+    return 0;
+}
+
+/*
+// My solution
 void reverse(int a[], int i, int j) {
     int temp;
     while (i < j) {
@@ -30,3 +70,4 @@ int main() {
     }
     return 0;
 }
+/*
