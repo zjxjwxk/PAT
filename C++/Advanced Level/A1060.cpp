16bfabd8470731d1e52f85f5ed281ae9716cb5a3
zjxjwxk <zjxjwxk@gmail.com>
2020-01-19 13:08:55 +0800

    update A1060

--- C++/Advanced Level/A1060.cpp
+++ C++/Advanced Level/A1060.cpp
@@ -3,46 +3,50 @@
 #include <iostream>
 using namespace std;
 
-void show_num(string str, int len, int n) {
-    if (n <= len) {
-        printf(" %s*10^%d", str.c_str(), len);
-    } else {
-        printf(" %s", str.c_str());
-        for (int i = 0; i < n - len; i++) {
-            printf("0");
-        }
-        printf("*10^%d", len);
+string transfer(string str, int& e, int n) {
+    while(str[0] == '0') {
+        str.erase(str.begin());
     }
+    if (str[0] == '.') {
+        str.erase(str.begin());
+        while (str[0] == '0') {
+            str.erase(str.begin());
+            e--;
+        }
+        if (str.length() == 0) {
+            e = 0;
+        }
+    } else {
+        int index = str.find(".");
+        if (index != string::npos) {
+            str.erase(index, 1);
+            e = index;
+        } else {
+            e = str.length();
+        }
+    }
+    int len = str.length();
+    while (len < n) {
+        str.insert(len, "0");
+        len++;
+    }
+    return str;
 }
 
 int main() {
     int n;
-    string a, b;
-    //scanf("%d %s %s", &n, &a[0], &b[0]);
-    cin >> n;
-    cin >> a;
-    cin >> b;
-    int len1 = a.length(), len2 = b.length();
-    int index1, index2;
-    if ((index1 = a.find(".")) != string::npos) {
-        a.erase(index1, 1);
-        len1 = index1;
-    }
-    if ((index2 = b.find(".")) != string::npos) {
-        b.erase(index2, 1);
-        len2 = index2;
-    }
-    a.insert(0, "0.");
-    b.insert(0, "0.");
-    a.erase(a.begin() + 2 + n, a.end());
-    b.erase(b.begin() + 2 + n, b.end());
-    if (len1 == len2 && a == b) {
-        printf("YES");
-        show_num(a, len1, n);
+    string a, b, str1, str2;
+    cin >> n >> a >> b;
+    int e1 = 0, e2 = 0;
+    str1 = transfer(a, e1, n);
+    str2 = transfer(b, e2, n);
+    str1 = str1.substr(0, n);
+    str2 = str2.substr(0, n);
+    if (str1 == str2 && e1 == e2) {
+        cout << "YES " << "0." << str1 << "*10^" << e1;
     } else {
-        printf("NO");
-        show_num(a, len1, n);
-        show_num(b, len2, n);
+        cout << "NO " << "0." << str1 << "*10^" << e1
+            << " 0." << str2 << "*10^" << e2;
     }
     return 0;
 }
