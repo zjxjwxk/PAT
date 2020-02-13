2a01e23e518f224cceaea7d56788bc536fad3584
zjxjwxk <zjxjwxk@gmail.com>
2020-02-13 22:12:42 +0800

    add A1072

---
+++ C++/Advanced Level/A1072.cpp
@@ -0,0 +1,94 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+const int maxn = 1011, INF = 1000000000;
+int n, m, k, ds, G[maxn][maxn], d[maxn];
+bool vis[maxn] = {false};
+
+int getId(char str[]) {
+    int num = 0, i = 0, len = strlen(str);
+    while (i < len) {
+        if (str[i] != 'G') {
+            num = num * 10 + (str[i] - '0');
+        }
+        i++;
+    }
+    if (str[0] == 'G') {
+        return n + num;
+    } else {
+        return num;
+    }
+}
+
+void Dijkstra(int gas) {
+    memset(vis, false, sizeof(vis));
+    fill(d, d + maxn, INF);
+    int index = gas + n;
+    d[index] = 0;
+    for (int i = 0; i < n + m; i++) {
+        int u = -1, min = INF;
+        for (int j = 1; j <= n + m; j++) {
+            if (vis[j] == false && d[j] < min) {
+                min = d[j];
+                u = j;
+            }
+        }
+        if (u == -1) {
+            return;
+        }
+        vis[u] = true;
+        for (int v = 1; v <= n + m; v++) {
+            if (vis[v] == false && G[u][v] != INF && d[u] + G[u][v] < d[v]) {
+                d[v] = d[u] + G[u][v];
+            }
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d %d %d", &n, &m, &k, &ds);
+    fill(G[0], G[0] + maxn * maxn, INF);
+    char str1[4], str2[4];
+    int v1, v2, dist;
+    for (int i = 0; i < k; i++) {
+        scanf("%s %s %d", str1, str2, &dist);
+        v1 = getId(str1);
+        v2 = getId(str2);
+        G[v1][v2] = G[v2][v1] = dist;
+    }
+    double min = -1, min_avg = INF;
+    int min_gas = -1;
+    for (int i = 1; i <= m; i++) {
+        Dijkstra(i);
+        double temp_min = INF, temp_avg = 0;
+        for (int j = 1; j <= n; j++) {
+            if (d[j] > ds) {
+                temp_min = -1;
+                break;
+            }
+            if (d[j] < temp_min) {
+                temp_min = d[j];
+            }
+            temp_avg += 1.0 * d[j] / n;
+        }
+        if (temp_min != -1) {
+            if (temp_min > min) {
+                min = temp_min;
+                min_gas = i;
+                min_avg = temp_avg;
+            } else if (temp_min == min && temp_avg < min_avg) {
+                min_gas = i;
+                min_avg = temp_avg;
+            }
+        }
+    }
+    if (min_gas == -1) {
+        printf("No Solution");
+    } else {
+        printf("G%d\n", min_gas);
+        printf("%.1f %.1f", min, min_avg);
+    }
+    return 0;
+}
